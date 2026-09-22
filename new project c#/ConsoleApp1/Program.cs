ccc929518c08ef5d7f39bd681d9d01e29d901311
akunpcfajar789-commits <akunpcfajar789@gmail.com>
2026-09-22 11:50:23 +0700

    Add files via upload

---
+++ new project c#/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿public class HelloWorld
+{
+     String halo = "halo";
+    int angkaSatu;
+    int angkaDua;
+
+    public void fungsiDalamKelas()
+    {
+        String haloJuga = "haloo";
+
+    }
+    public void operasiTambah(int angkaSatuPar, int angkaDuaPar)
+    {
+        this.angkaSatu = angkaSatuPar;
+        this.angkaDua = angkaDuaPar;
+        Console.WriteLine(angkaSatuPar + angkaDuaPar);
+
+    }
+     public void operasikurang(int angkaSatuPar, int angkaDuaPar)
+    {
+        this.angkaSatu = angkaSatuPar;
+        this.angkaDua = angkaDuaPar;
+        Console.WriteLine(angkaSatuPar - angkaDuaPar);
+
+    }
+     public void operasiKali(int angkaSatuPar, int angkaDuaPar)
+    {
+        this.angkaSatu = angkaSatuPar;
+        this.angkaDua = angkaDuaPar;
+        Console.WriteLine(angkaSatuPar * angkaDuaPar);
+
+    }
+      public void operasiBagi(int angkaSatuPar, int angkaDuaPar)
+    {
+        this.angkaSatu = angkaSatuPar;
+        this.angkaDua = angkaDuaPar;
+        Console.WriteLine(angkaSatuPar / angkaDuaPar);
+
+    }
+
+
+
+    public static void Main()
+    {
+        Console.WriteLine("Merdeka");
+        HelloWorld inisiasiHello = new HelloWorld();
+
+        Console.WriteLine(inisiasiHello.halo);
+
+        inisiasiHello.fungsiDalamKelas();
+
+        inisiasiHello.operasiTambah(2,1);
+        inisiasiHello.operasikurang(4,2);
+        inisiasiHello.operasiKali(3,2);
+        inisiasiHello.operasiBagi(4,2);
+    }
+
+}
